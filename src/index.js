84ba10d17b611773e323a235c7d04370a2f1566a
e2tha-e <ee@electric-eloquence.io>
2019-03-12 23:22:04 -0400

    commenting on unicodes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+// The \u0002 and \u0003 unicodes could be replaced with variables, but it is more clear what they are and what their
+// intent is if left as unicode. They are respectively Start of Text and End of Text characters. Their purpose is to be
+// temporary alternate tag delimiters.
+
 'use strict';
 
 const hogan = require('../lib/hogan.js/lib/hogan.js');
