71a33aab6a34b75d7ead03698bc688fdcb956d55
e2tha-e <ee@electric-eloquence.io>
2020-03-21 09:14:47 -0400

    bundle update

--- dist/feplet.node.es5.js
+++ dist/feplet.node.es5.js
@@ -14,7 +14,7 @@
 // browsers. Given that this is a relatively small file, it is easier to keep the code in one file.
 'use strict';
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 var hogan = require('../lib/hogan.js/lib/hogan.js');
 
