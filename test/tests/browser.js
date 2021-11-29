6d8c9b98770cbbe533ce8e19f5497e843f2a1fc5
electric-el <electric-el@electric-eloquence.io>
2021-11-29 06:17:35 -0500

    Squashed commit of the following:
    commit 082db7ddafd7017353ed273c25ccf6195e9821ea
    Author: electric-el <electric-el@electric-eloquence.io>
    Date:   Mon Nov 29 06:14:49 2021 -0500
    
        typo fix
    
    commit 3c226982a0a67c955771d095426b68925b504e6c
    Author: electric-el <electric-el@electric-eloquence.io>
    Date:   Mon Nov 29 06:13:44 2021 -0500
    
        Revert "refactored async e2e tests"
    
        This reverts commit 166c1791c16ed6286707ca5bfb743cd8b24d72f1.
    
    commit 9de3687f0b01dfb9e04844dc7dfb4aaa8a4084b5
    Author: electric-el <electric-el@electric-eloquence.io>
    Date:   Mon Nov 29 06:13:40 2021 -0500
    
        Revert "upped browser test increment"
    
        This reverts commit b0872d9c633a97a3f74c361b9d5f346345cbed1d.
    
    commit 8370bc78b5376ed70ae632cb78bcb166185294a2
    Author: electric-el <electric-el@electric-eloquence.io>
    Date:   Mon Nov 29 06:13:36 2021 -0500
    
        Revert "upped browser test increment"
    
        This reverts commit 2fac829cbf9cfd0e4603042965eb4e7f0ffa6631.
    
    commit b56339e3df7932b7fb09ac37e0e1db9895839c93
    Author: electric-el <electric-el@electric-eloquence.io>
    Date:   Mon Nov 29 06:13:33 2021 -0500
    
        Revert "refactored async e2e tests"
    
        This reverts commit f11b9b7edc0e53f0b0631ff1c113f7499fef3083.
    
    commit a0a5e175bfea9f42b1445a0a7938f017f2c6fb83
    Author: electric-el <electric-el@electric-eloquence.io>
    Date:   Mon Nov 29 06:13:29 2021 -0500
    
        Revert "refactored async e2e tests"
    
        This reverts commit b70624e15fa93a288e4d80b9d431626ded3f6a60.
    
    commit 73122cf1971e1d4c447e7d82f8e21579f87f8bec
    Author: electric-el <electric-el@electric-eloquence.io>
    Date:   Mon Nov 29 06:13:25 2021 -0500
    
        Revert "upped browser test increment"
    
        This reverts commit f5e70bb4787089c159a59944bbebedbcc94fef62.
    
    commit 4bd4f61677343d28b654a00748fa83edceaae80e
    Author: electric-el <electric-el@electric-eloquence.io>
    Date:   Mon Nov 29 06:13:21 2021 -0500
    
        Revert "upped browser test increment"
    
        This reverts commit eb980d0e0a28b9add706d5fdbae733e19b8b00f4.

--- test/tests/browser.js
+++ test/tests/browser.js
@@ -6,317 +6,283 @@ let delay = 0;
 
 module.exports = () => {
   it('0. Hydrates templates with variables', async () => {
-    const assertion0 = await $('.assertion-0');
     delay += increment;
     await browser.pause(delay);
-    const assertion0Html = await assertion0.getHTML(false);
 
-    expect(assertion0Html).to.equal(expectation[0]);
+    const assertion0 = await (await $('.assertion-0')).getHTML(false);
+    expect(assertion0).to.equal(expectation[0]);
   });
 
   it('1. Hydrates templates with nested variables', async () => {
-    const assertion1 = await $('.assertion-1');
     delay += increment;
     await browser.pause(delay);
-    const assertion1Html = await assertion1.getHTML(false);
 
-    expect(assertion1Html).to.equal(expectation[1]);
+    const assertion1 = await (await $('.assertion-1')).getHTML(false);
+    expect(assertion1).to.equal(expectation[1]);
   });
 
   it('2. Recursively hydrates templates with variables', async () => {
-    const assertion2 = await $('.assertion-2');
     delay += increment;
     await browser.pause(delay);
-    const assertion2Html = await assertion2.getHTML(false);
 
-    expect(assertion2Html).to.equal(expectation[2]);
+    const assertion2 = await (await $('.assertion-2')).getHTML(false);
+    expect(assertion2).to.equal(expectation[2]);
   });
 
   it('3. Recursively hydrates templates with nested variables', async () => {
-    const assertion3 = await $('.assertion-3');
     delay += increment;
     await browser.pause(delay);
-    const assertion3Html = await assertion3.getHTML(false);
 
-    expect(assertion3Html).to.equal(expectation[3]);
+    const assertion3 = await (await $('.assertion-3')).getHTML(false);
+    expect(assertion3).to.equal(expectation[3]);
   });
 
   it('4. Hydrates variables written in dot.notation', async () => {
-    const assertion4 = await $('.assertion-4');
+    const assertion4 = await (await $('.assertion-4')).getHTML(false);
     delay += increment;
-    await browser.pause(delay);
-    const assertion4Html = await assertion4.getHTML(false);
 
-    expect(assertion4Html).to.equal(expectation[4]);
+    await browser.pause(delay);
+    expect(assertion4).to.equal(expectation[4]);
   });
 
   it('5. Hydrates variables within an array written in dot.notation', async () => {
-    const assertion5 = await $('.assertion-5');
     delay += increment;
     await browser.pause(delay);
-    const assertion5Html = await assertion5.getHTML(false);
 
-    expect(assertion5Html).to.equal(expectation[5]);
+    const assertion5 = await (await $('.assertion-5')).getHTML(false);
+    expect(assertion5).to.equal(expectation[5]);
   });
 
   it('6. Recursively hydrates variables written in dot.notation', async () => {
-    const assertion6 = await $('.assertion-6');
     delay += increment;
     await browser.pause(delay);
-    const assertion6Html = await assertion6.getHTML(false);
 
-    expect(assertion6Html).to.equal(expectation[6]);
+    const assertion6 = await (await $('.assertion-6')).getHTML(false);
+    expect(assertion6).to.equal(expectation[6]);
   });
 
   it('7. Recursively hydrates variables within an array written in dot.notation', async () => {
-    const assertion7 = await $('.assertion-7');
     delay += increment;
     await browser.pause(delay);
-    const assertion7Html = await assertion7.getHTML(false);
 
-    expect(assertion7Html).to.equal(expectation[7]);
+    const assertion7 = await (await $('.assertion-7')).getHTML(false);
+    expect(assertion7).to.equal(expectation[7]);
   });
 
   it('9. Hydrates templates with variables passed per the Pattern Lab styleModifier convention', async () => {
-    const assertion9 = await $('.assertion-9');
     delay += increment;
     await browser.pause(delay);
-    const assertion9Html = await assertion9.getHTML(false);
 
-    expect(assertion9Html).to.equal(expectation[9]);
+    const assertion9 = await (await $('.assertion-9')).getHTML(false);
+    expect(assertion9).to.equal(expectation[9]);
   });
 
   it('10. Recursively hydrates templates with variables passed per the Pattern Lab styleModifier convention\
 ', async () => {
-    const assertion10 = await $('.assertion-10');
     delay += increment;
     await browser.pause(delay);
-    const assertion10Html = await assertion10.getHTML(false);
 
-    expect(assertion10Html).to.equal(expectation[10]);
+    const assertion10 = await (await $('.assertion-10')).getHTML(false);
+    expect(assertion10).to.equal(expectation[10]);
   });
 
   it('11. Hydrates templates with multiple classes passed per Pattern Lab styleModifier', async () => {
-    const assertion11 = await $('.assertion-11');
     delay += increment;
     await browser.pause(delay);
-    const assertion11Html = await assertion11.getHTML(false);
 
-    expect(assertion11Html).to.equal(expectation[11]);
+    const assertion11 = await (await $('.assertion-11')).getHTML(false);
+    expect(assertion11).to.equal(expectation[11]);
   });
 
   it('12. Hydrates templates with both data parameters and a Pattern Lab styleModifier', async () => {
-    const assertion12 = await $('.assertion-12');
     delay += increment;
     await browser.pause(delay);
-    const assertion12Html = await assertion12.getHTML(false);
 
-    expect(assertion12Html).to.equal(expectation[12]);
+    const assertion12 = await (await $('.assertion-12')).getHTML(false);
+    expect(assertion12).to.equal(expectation[12]);
   });
 
   it('13. Hydrates templates with both data parameters and a styleModifier with multiple classes', async () => {
-    const assertion13 = await $('.assertion-13');
     delay += increment;
     await browser.pause(delay);
-    const assertion13Html = await assertion13.getHTML(false);
 
-    expect(assertion13Html).to.equal(expectation[13]);
+    const assertion13 = await (await $('.assertion-13')).getHTML(false);
+    expect(assertion13).to.equal(expectation[13]);
   });
 
   it('14. Recursively hydrates templates with multiple classes passed per Pattern Lab styleModifier', async () => {
-    const assertion14 = await $('.assertion-14');
     delay += increment;
     await browser.pause(delay);
-    const assertion14Html = await assertion14.getHTML(false);
 
-    expect(assertion14Html).to.equal(expectation[14]);
+    const assertion14 = await (await $('.assertion-14')).getHTML(false);
+    expect(assertion14).to.equal(expectation[14]);
   });
 
   it('15. Recursively hydrates templates with both data parameters and a Pattern Lab styleModifier', async () => {
-    const assertion15 = await $('.assertion-15');
     delay += increment;
     await browser.pause(delay);
-    const assertion15Html = await assertion15.getHTML(false);
 
-    expect(assertion15Html).to.equal(expectation[15]);
+    const assertion15 = await (await $('.assertion-15')).getHTML(false);
+    expect(assertion15).to.equal(expectation[15]);
   });
 
   it('16. Recursively hydrates templates with both data parameters and a styleModifier with multiple classes\
 ', async () => {
-    const assertion16 = await $('.assertion-16');
     delay += increment;
     await browser.pause(delay);
-    const assertion16Html = await assertion16.getHTML(false);
 
-    expect(assertion16Html).to.equal(expectation[16]);
+    const assertion16 = await (await $('.assertion-16')).getHTML(false);
+    expect(assertion16).to.equal(expectation[16]);
   });
 
   it('17. Shuts off otherwise infinite recursion paths with default false conditions', async () => {
-    const assertion17 = await $('.assertion-17');
     delay += increment;
     await browser.pause(delay);
-    const assertion17Html = await assertion17.getHTML(false);
 
-    expect(assertion17Html).to.equal(expectation[17]);
+    const assertion17 = await (await $('.assertion-17')).getHTML(false);
+    expect(assertion17).to.equal(expectation[17]);
   });
 
   it('18. Shuts off otherwise infinite recursion paths when flagged to do so by parameters', async () => {
-    const assertion18 = await $('.assertion-18');
     delay += increment;
     await browser.pause(delay);
-    const assertion18Html = await assertion18.getHTML(false);
 
-    expect(assertion18Html).to.equal(expectation[18]);
+    const assertion18 = await (await $('.assertion-18')).getHTML(false);
+    expect(assertion18).to.equal(expectation[18]);
   });
 
   it('23. Renders a nested parameter variable differently than a non-parameter variable of the same name\
 ', async () => {
-    const assertion23 = await $('.assertion-23');
     delay += increment;
     await browser.pause(delay);
-    const assertion23Html = await assertion23.getHTML(false);
 
-    expect(assertion23Html).to.equal(expectation[23]);
+    const assertion23 = await (await $('.assertion-23')).getHTML(false);
+    expect(assertion23).to.equal(expectation[23]);
   });
 
   it('24. Renders an array of nested parameter variables differently from non-parameter variables of the same name\
 ', async () => {
-    const assertion24 = await $('.assertion-24');
     delay += increment;
     await browser.pause(delay);
-    const assertion24Html = await assertion24.getHTML(false);
 
-    expect(assertion24Html).to.equal(expectation[24]);
+    const assertion24 = await (await $('.assertion-24')).getHTML(false);
+    expect(assertion24).to.equal(expectation[24]);
   });
 
   it('25. Renders a more deeply nested parameter variable differently then a non-parameter variable of the same name\
 ', async () => {
-    const assertion25 = await $('.assertion-25');
     delay += increment;
     await browser.pause(delay);
-    const assertion25Html = await assertion25.getHTML(false);
 
-    expect(assertion25Html).to.equal(expectation[25]);
+    const assertion25 = await (await $('.assertion-25')).getHTML(false);
+    expect(assertion25).to.equal(expectation[25]);
   });
 
   it('26. Renders a deeply nested dot.notation parameter differently than a non-parameter variable of the same name\
 ', async () => {
-    const assertion26 = await $('.assertion-26');
     delay += increment;
     await browser.pause(delay);
-    const assertion26Html = await assertion26.getHTML(false);
 
-    expect(assertion26Html).to.equal(expectation[26]);
+    const assertion26 = await (await $('.assertion-26')).getHTML(false);
+    expect(assertion26).to.equal(expectation[26]);
   });
 
   it('27. Renders a deeply nested array of dot.notation parameters differently than non-parameter variables of the \
 same name', async () => {
-    const assertion27 = await $('.assertion-27');
     delay += increment;
     await browser.pause(delay);
-    const assertion27Html = await assertion27.getHTML(false);
 
-    expect(assertion27Html).to.equal(expectation[27]);
+    const assertion27 = await (await $('.assertion-27')).getHTML(false);
+    expect(assertion27).to.equal(expectation[27]);
   });
 
   it('28. Renders a moderately nested dot.notation parameter differently than a non-parameter variable of the same \
 name', async () => {
-    const assertion28 = await $('.assertion-28');
     delay += increment;
     await browser.pause(delay);
-    const assertion28Html = await assertion28.getHTML(false);
 
-    expect(assertion28Html).to.equal(expectation[28]);
+    const assertion28 = await (await $('.assertion-28')).getHTML(false);
+    expect(assertion28).to.equal(expectation[28]);
   });
 
   it('29. Renders a moderately nested array of dot.notation parameters differently than non-parameter variables of \
 the same name', async () => {
-    const assertion29 = await $('.assertion-29');
     delay += increment;
     await browser.pause(delay);
-    const assertion29Html = await assertion29.getHTML(false);
 
-    expect(assertion29Html).to.equal(expectation[29]);
+    const assertion29 = await (await $('.assertion-29')).getHTML(false);
+    expect(assertion29).to.equal(expectation[29]);
   });
 
   it('34. Renders a top-level dot.notation parameter that nests more tags', async () => {
-    const assertion34 = await $('.assertion-34');
     delay += increment;
     await browser.pause(delay);
-    const assertion34Html = await assertion34.getHTML(false);
 
-    expect(assertion34Html).to.equal(expectation[34]);
+    const assertion34 = await (await $('.assertion-34')).getHTML(false);
+    expect(assertion34).to.equal(expectation[34]);
   });
 
   it('35. Renders an array of top-level dot.notation parameters that nest more tags', async () => {
-    const assertion35 = await $('.assertion-35');
     delay += increment;
     await browser.pause(delay);
-    const assertion35Html = await assertion35.getHTML(false);
 
-    expect(assertion35Html).to.equal(expectation[35]);
+    const assertion35 = await (await $('.assertion-35')).getHTML(false);
+    expect(assertion35).to.equal(expectation[35]);
   });
 
   it('36. Renders a dot.notation parameter nested within a non-parameter', async () => {
-    const assertion36 = await $('.assertion-36');
     delay += increment;
     await browser.pause(delay);
-    const assertion36Html = await assertion36.getHTML(false);
 
-    expect(assertion36Html).to.equal(expectation[36]);
+    const assertion36 = await (await $('.assertion-36')).getHTML(false);
+    expect(assertion36).to.equal(expectation[36]);
   });
 
   it('37. Renders an array of dot.notation parameters nested within a non-parameter', async () => {
-    const assertion37 = await $('.assertion-37');
     delay += increment;
     await browser.pause(delay);
-    const assertion37Html = await assertion37.getHTML(false);
 
-    expect(assertion37Html).to.equal(expectation[37]);
+    const assertion37 = await (await $('.assertion-37')).getHTML(false);
+    expect(assertion37).to.equal(expectation[37]);
   });
 
   it('38. Renders dot.notation parameters nested aside each other within a non-parameter', async () => {
-    const assertion38 = await $('.assertion-38');
     delay += increment;
     await browser.pause(delay);
-    const assertion38Html = await assertion38.getHTML(false);
 
-    expect(assertion38Html).to.equal(expectation[38]);
+    const assertion38 = await (await $('.assertion-38')).getHTML(false);
+    expect(assertion38).to.equal(expectation[38]);
   });
 
   it('39. Renders dot.notation parameter nested within another within a non-parameter', async () => {
-    const assertion39 = await $('.assertion-39');
     delay += increment;
     await browser.pause(delay);
-    const assertion39Html = await assertion39.getHTML(false);
 
-    expect(assertion39Html).to.equal(expectation[39]);
+    const assertion39 = await (await $('.assertion-39')).getHTML(false);
+    expect(assertion39).to.equal(expectation[39]);
   });
 
   it('43. Renders a deeply nested dot.notation parameter containing an array', async () => {
-    const assertion43 = await $('.assertion-43');
     delay += increment;
     await browser.pause(delay);
-    const assertion43Html = await assertion43.getHTML(false);
 
-    expect(assertion43Html).to.equal(expectation[43]);
+    const assertion43 = await (await $('.assertion-43')).getHTML(false);
+    expect(assertion43).to.equal(expectation[43]);
   });
 
   it('44. Renders a moderately nested dot.notation parameter containing an array', async () => {
-    const assertion44 = await $('.assertion-44');
     delay += increment;
     await browser.pause(delay);
-    const assertion44Html = await assertion44.getHTML(false);
 
-    expect(assertion44Html).to.equal(expectation[44]);
+    const assertion44 = await (await $('.assertion-44')).getHTML(false);
+    expect(assertion44).to.equal(expectation[44]);
   });
 
   it('45. Renders a top-level dot.notation parameter containing an array', async () => {
-    const assertion45 = await $('.assertion-45');
     delay += increment;
     await browser.pause(delay);
-    const assertion45Html = await assertion45.getHTML(false);
 
-    expect(assertion45Html).to.equal(expectation[45]);
+    const assertion45 = await (await $('.assertion-45')).getHTML(false);
+    expect(assertion45).to.equal(expectation[45]);
   });
 };
